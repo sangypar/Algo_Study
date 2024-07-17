b2da6c39ef525c97b8c152dc76c006e968ffe655
김유석 <kurladbtjr@naver.com>
2024-07-17 17:48:44 +0900

    [프로그래머스]_주차요금계산

--- 김유석/[240717]_프로그래머스_주차요금계산 (2).java
+++ 김유석/[240717]_프로그래머스_주차요금계산 (2).java
@@ -1,5 +1,22 @@
 import java.util.*;
-
+/*
+ * 테스트 1 〉	통과 (4.16ms, 74.1MB)
+테스트 2 〉	통과 (2.50ms, 79.7MB)
+테스트 3 〉	통과 (1.96ms, 78.1MB)
+테스트 4 〉	통과 (2.33ms, 80.4MB)
+테스트 5 〉	통과 (4.15ms, 85.3MB)
+테스트 6 〉	통과 (4.12ms, 88.3MB)
+테스트 7 〉	통과 (21.89ms, 82.8MB)
+테스트 8 〉	통과 (11.80ms, 81.4MB)
+테스트 9 〉	통과 (3.08ms, 77.7MB)
+테스트 10 〉	통과 (18.75ms, 87.5MB)
+테스트 11 〉	통과 (11.83ms, 79.6MB)
+테스트 12 〉	통과 (8.18ms, 81.7MB)
+테스트 13 〉	통과 (2.51ms, 75.4MB)
+테스트 14 〉	통과 (2.72ms, 78.8MB)
+테스트 15 〉	통과 (2.35ms, 78.4MB)
+테스트 16 〉	통과 (1.99ms, 73.1MB)
+ */
 public class Programmers_6_주차요금계산3 {
     public static void main(String[] args) {
         int[] fees = {180, 5000, 10, 600}; // 기본시간, 기본요금, 단위시간, 단위요금
@@ -30,7 +47,7 @@ public class Programmers_6_주차요금계산3 {
         }
 
         Set<Integer> carNumbers = new HashSet<>();
-
+        
         for (String record : records) {
             String[] splited = record.split(" ");
             String time = splited[0];
@@ -54,9 +71,11 @@ public class Programmers_6_주차요금계산3 {
             int totalMinutes = 0;
             for (int j = 0; j < history.size(); j++) {
                 carManager car = history.get(j);
-                if (car.isIn) {
+                if (car.isIn) { //24시 까지 있었던것으로 간주  
+                	//2번 안들어왔으면 false -> 24시 간주 초기화
+                	//2번 들어왔으면 true  -> 아웃타임 초기화
                     int outTime = (j + 1 < history.size() && !history.get(j + 1).isIn) ? history.get(j + 1).time : 23 * 60 + 59;
-                    totalMinutes += (outTime - car.time);
+                    totalMinutes += (outTime - car.time); 
                 }
             }
 
