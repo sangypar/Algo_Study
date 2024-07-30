423f5ea4f080099454fd3ade56967a37a7fe3e7b
박상용 <psyok7@naver.com>
2024-07-30 23:12:33 +0900

    [백준] 12904_A와 B

---
+++ 박상용/7월/[240730] 12904 A와B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PSY
+{
+    internal class Class12
+    {
+        static void Main()
+        {
+            string S = Console.ReadLine();
+            string T = Console.ReadLine();
+
+            while (T.Length > S.Length)
+                T = (T[T.Length - 1] == 'A') ? T.Remove(T.Length - 1) : new string(T.Remove(T.Length - 1).Reverse().ToArray());
+
+            Console.WriteLine(S == T ? 1 : 0);
+        }
+    }
+}
